8ccc016a26e9f8aa8ace3d103b5bf403be9329dd
David Robinson <dr6@sanger.ac.uk>
2017-03-03 14:57:30 +0000

    Added spec for SetClient

--- spec/set_client_spec.rb
+++ spec/set_client_spec.rb
@@ -5,7 +5,21 @@ describe SetClient do
     expect(SetClient::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a uuid" do
+    expect(SetClient::Set.new).to respond_to :uuid
+  end
+
+  it "is connected to the json api" do
+    url='http://localhost:9999/api/v1/'
+    id="123"
+    SetClient::Base.site = url
+
+    stub_request(:get, url+"sets/"+id).
+         with(headers: {'Accept'=>'application/vnd.api+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/vnd.api+json', 'User-Agent'=>'Faraday v0.11.0'}).
+         to_return(status: 200, body: "", headers: {})
+    stub_request(:get, url+'/sets')
+        .to_return(status: 200, body: {id: id}.to_json, headers: {})
+
+    expect(SetClient::Set.find(id)).not_to be nil
   end
 end
